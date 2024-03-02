74eb7723b09adbd24d196abae5652b611b74043a
Ahmed Ehab5006 <ahm.ehab1510@gmail.com>
2024-03-02 15:15:37 +0200

    video title and quality menu added

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 from tkinter import *
 from customtkinter import *
 import customtkinter 
-from pytube import *
+from pytube import YouTube
+from tkinter import messagebox as msg
 
 
 link = " " #creating a variable to store video's link
+videoName = " " #creating a variable to store video's name
+ #creating a variable to store video's stream
+
+
 
 root = CTk () #creating the root window
 
@@ -13,18 +18,31 @@ root = CTk () #creating the root window
 
 #creatung function which creates the download tab
 def downloadTab():
-    global linkEntry , youtubeLogo , titleLabel , downloadButton , backButton
+    global linkEntry , youtubeLogo , titleLabel , downloadButton , backButton , videoName , videoNameLabel , streams , quality
     blank_label = customtkinter.CTkLabel(root, text="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", text_color="#242424")
     blank_label.place(x=190, y=440)
     youtubeLogo.destroy()
     downloadButton.destroy()
     linkEntry.destroy()
     titleLabel.destroy()
-    
-    
+    titleLabel = CTkLabel(root, text="YouTube Video Downloader" , text_color="white" , font=("Times New Roman", 20, "bold") )
+    titleLabel.place(x=135, y=20)
+    youtubeLogoImage = PhotoImage(file="Elements\\youtube.png")
+    youtubeLogo = CTkLabel(root, image=youtubeLogoImage , text = " ")
+    youtubeLogo.place(x=119, y=40)
+    videoNameLabel = CTkLabel(root, text = videoName  ,  text_color="white" , font=("Times New Roman", 15))
+    videoNameLabel.place(x=35, y=250)
+    stream = list(streams)
+    quality = customtkinter.CTkOptionMenu(master = root , values = stream , width = 50  )
+    quality.place(x=330, y=250)
     
     #creating function which back to the main tab
     def back():
+        global  youtubeLogo , titleLabel ,videoNameLabel
+        titleLabel.destroy()
+        youtubeLogo.destroy()
+        videoNameLabel.destroy()
+        quality.destroy()
         mainTab()
         backButton.destroy()
     backButton = CTkButton(root, text="Back", font=("Times New Roman", 15, "bold") , width=50 , command=back)
@@ -34,8 +52,7 @@ def downloadTab():
 
 #creating function which stores video's link and then redirects to the download tab
 def Entry ():
-    global link
-    global linkEntry
+    global link , linkEntry , videoName , streams 
     link = linkEntry.get()
     if link == "":
         errorLabel = CTkLabel(root, text = "Please enter a link" , text_color="red" , font=("Times New Roman", 17, "bold") )
@@ -46,7 +63,20 @@ def Entry ():
         errorLabel = CTkLabel(root, text = "Invalid link" , text_color="red" , font=("Times New Roman", 17, "bold") )
         errorLabel.place(x=218, y=440)
     else :
-        downloadTab()    
+        try :
+            #blank_label = customtkinter.CTkLabel(root, text="please wait....", text_color="white")
+            #blank_label.place(x=190, y=440)
+            yt = YouTube(link)
+            yt.streams.get_lowest_resolution()
+            video = yt.streams.get_lowest_resolution()
+            videoName = str(video.default_filename)
+            streams = set()
+            for stream in yt.streams.filter(type="video"):  # Only look for video streams to avoid None values
+                streams.add(stream.resolution)
+            downloadTab()  
+        except:
+            msg.showerror(title = "YouTube Video Downloader ", message = "An error occured (this may be caused due to bad internet connection or the video may be unavailable)")
+               
        
 
 
